1d61d8701afc88c5fc2997327b426562eea98867
Justine Hoffa <justinefarrer@yahoo.com>
2020-09-17 08:06:24 -0600

    Add project files.

---
+++ RolloftheDice.vb
@@ -0,0 +1,20 @@
+﻿'Justine Hoffa
+'RCET0265
+'Fall2020
+'RolloftheDice
+'
+
+Option Strict On
+Option Explicit On
+Option Compare Text
+
+Module RolloftheDice
+
+    Sub Main()
+
+
+
+
+    End Sub
+
+End Module
